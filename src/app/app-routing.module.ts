e88a0e4efb32ed2a5d9b42e015dbad01e681bc4f
Rickey Shumway <eric.g.shumway@gmail.com>
2021-09-14 08:22:51 -0600

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 const routes: Routes = [
-  {path: '', component: LandingComponent},
-  {path: '/login', component: LoginComponent},
+  
+    {path: '', component: LandingComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'search', component: SearchComponent}
+  
+  
 ];
 
 @NgModule({
